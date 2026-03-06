f956b2637d5d32676e2f25c466cd0f7176438eef
Mayank malviya <mayankmalviya440@gmail.com>
2026-03-06 22:31:03 +0530

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,2 +1,43 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
 
+public class Client {
+
+    private static final String SERVER_ADDRESS = "localhost";
+    private static final int PORT = 1234;
+
+    public static void main(String[] args) {
+
+        try {
+
+            Socket socket = new Socket(SERVER_ADDRESS, PORT);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+
+            PrintWriter out = new PrintWriter(
+                    socket.getOutputStream(), true);
+
+            Scanner scanner = new Scanner(System.in);
+
+            new Thread(() -> {
+                try {
+                    String response;
+                    while ((response = in.readLine()) != null) {
+                        System.out.println(response);
+                    }
+                } catch (IOException e) {
+                }
+            }).start();
+
+            while (scanner.hasNextLine()) {
+                out.println(scanner.nextLine());
+            }
+
+        } catch (IOException e) {
+            System.out.println("Unable to connect to server.");
+        }
+    }
+}
 
